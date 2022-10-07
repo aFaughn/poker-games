cf7b84f2c9c8afedd366ee61a2f55df9a7b586cb
allen <allenfaughn@gmail.com>
2022-10-07 11:07:45 -0500

    finished backend error handling

--- backend/app.js
+++ backend/app.js
@@ -46,24 +46,38 @@ app.use(
 const routes = require('./routes')
 app.use(routes)
 
-// Error Handling middle-ware
-const { ValidationError } = require('sequelize')
 
-// 404
-app.use((_req, _res, next) => {
-    const err = new Error('The requested resource couldn\'t be found.');
-    err.title = "Resource Not Found"
-    err.errors = ["The requested resource could not be found."]
-    err.status = 404;
-    next(err);
-})
+//Error Handling middle-ware
+    const { ValidationError } = require('sequelize')
 
-app.use((err, _req, _res, next) => {
-    if (err instanceof ValidationError) {
-        err.errors = err.errors.map((e) => e.message);
-        err.title = 'Validation error';
-    }
-    next(err);
-});
+    // 404
+    app.use((_req, _res, next) => {
+        const err = new Error('The requested resource couldn\'t be found.');
+        err.title = "Resource Not Found"
+        err.errors = ["The requested resource could not be found."]
+        err.status = 404;
+        next(err);
+    })
+
+    // Sequelize error
+    app.use((err, _req, _res, next) => {
+        if (err instanceof ValidationError) {
+            err.errors = err.errors.map((e) => e.message);
+            err.title = 'Validation error';
+        }
+        next(err);
+    });
+
+    // Error Formatter
+    app.use((err, _req, res, _next) => {
+        res.status(err.status || 500);
+        console.error(err);
+        res.json({
+            title: err.title || 'Server Error',
+            message: err.message,
+            errors: err.errors,
+            stack: isProduction ? null : err.stack,
+        });
+    });
 
 module.exports = app;
